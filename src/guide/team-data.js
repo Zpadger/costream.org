82ddfb1eec2969fbbf99fe9e38b540a8bb865fc3
Zpadger <31385823+Zpadger@users.noreply.github.com>
2018-12-08 11:10:17 +0800

    Update team-data.js

--- src/guide/team-data.js
+++ src/guide/team-data.js
@@ -328,4 +328,22 @@ team = team.concat(shuffle([ //shuffle 函数打乱了后续成员的顺序
       'https://github.com/zzNire'
     ]
   },
+  {
+    name: 'Peiji Zhang',
+    title: 'Member of DML',
+    city: 'Wuhan, China',
+    languages: ['zh', 'en'],
+    github: 'Zpadger',
+    twitter: null,
+    work: {
+      role: 'Master',
+      org: 'HUST DML'
+    },
+    reposOfficial: [
+      'COStream'
+    ],
+    links: [
+      'https://github.com/Zpadger'
+    ]
+  },
 ]))
